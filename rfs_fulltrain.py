d4bc7edd7c89c4e131c4490feffb738e2cd74752
Katy Scott <k.l.scott16@gmail.com>
2021-09-27 14:39:23 -0400

    Added saving the model

--- rfs_fulltrain.py
+++ rfs_fulltrain.py
@@ -2,31 +2,31 @@ import argparse
 from datetime import datetime
 import json
 import os
-import optuna
-from optuna.trial import TrialState
-import pickle
-from sklearn.model_selection import KFold
+# import optuna
+# from optuna.trial import TrialState
+# import pickle
+# from sklearn.model_selection import KFold
 from skimage.color import gray2rgb
-import torch.optim as optim
-from torch.utils.data import DataLoader
+# import torch.optim as optim
+# from torch.utils.data import DataLoader
 
 from rfs_preprocessing import *
 from rfs_utils import *
 from rfs_models import *
 
 parser = argparse.ArgumentParser(description='Training variables:')
-parser.add_argument('--batchsize', default=32, type=int, help='Number of samples used for each training cycle')
+parser.add_argument('--batchsize', default=16, type=int, help='Number of samples used for each training cycle')
 # parser.add_argument('--covariate', default=18, type=int, help='Number of genes flags in gene feature data')
 parser.add_argument('--datadir', default='/media/katy/Data/ICC/Data', type=str, help='Full path to the Data directory '
                                                                                      'where images, labels, are stored'
                                                                                      'and output will be saved.')
-parser.add_argument('--epochs', default=10, type=int, help='Number of training epochs to run')
+parser.add_argument('--epochs', default=100, type=int, help='Number of training epochs to run')
 parser.add_argument('--imdim', default=256, type=int, help='Dimension of image to load')
-parser.add_argument('--learnrate', default=3e-3, type=float, help='Starting learning rate for training')
-parser.add_argument('--modelname', default='Resnet18', type=str, help='Name of model type to use for CNN half of model.'
+parser.add_argument('--learnrate', default=1e-5, type=float, help='Starting learning rate for training')
+parser.add_argument('--modelname', default='Resnet34', type=str, help='Name of model type to use for CNN half of model.'
                                                                        'Current options are Resnet18 and Resnet34')
 parser.add_argument('--randseed', default=16, type=int, help='Random seed for reproducibility')
-parser.add_argument('--scanthresh', default=500, type=int, help='Threshold for number of tumour pixels to filter images'
+parser.add_argument('--scanthresh', default=300, type=int, help='Threshold for number of tumour pixels to filter images'
                                                                 ' through')
 parser.add_argument('--validation', default=0, type=int, help='Select validation method from list: '
                                                               '0: hold out, 1: k-fold')
@@ -158,6 +158,8 @@ def main():
     if args.plots:
         saveplot_coxloss(save_eval_fname, model._get_name())
         saveplot_concordance(save_eval_fname, model._get_name())
+
+    savemodel(out_path, model)
 
 
 if __name__ == '__main__':
