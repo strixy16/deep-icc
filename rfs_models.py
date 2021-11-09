90df6caf1aa1a2e6fdadb5361583748586926dfd
Katy Scott <k.l.scott16@gmail.com>
2021-11-09 15:39:36 -0500

    Fixed LeNet, had to remove padding from Conv and stride from pooling

--- rfs_models.py
+++ rfs_models.py
@@ -21,12 +21,12 @@ class LeNet5(nn.Module):
         super(LeNet5, self).__init__()
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
             nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=2),
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
             nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=2),
             nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
             nn.Tanh()
         )
@@ -34,7 +34,7 @@ class LeNet5(nn.Module):
         self.classifier = nn.Sequential(
             nn.Linear(in_features=120, out_features=84),
             nn.Tanh(),
-            nn.Linear(in_features=84, out_features=n_classes)
+            nn.Linear(in_features=84, out_features=n_classes),
         )
 
     def forward(self, x):
