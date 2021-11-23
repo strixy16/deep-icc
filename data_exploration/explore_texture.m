93c8a30d24ed07ec90420d5191a2953585708c02
Katy Scott <k.l.scott16@gmail.com>
2021-11-23 10:19:37 -0500

    Running linear CPH with train data only

--- data_exploration/explore_texture.m
+++ data_exploration/explore_texture.m
@@ -29,6 +29,8 @@ tbl_allfeat = tbl_allfeat(:, 2:end);
 
 % Adding RFS labels to the table and getting only patients with RFS label
 tbl_featandlabels = innerjoin(tbl_RFS, tbl_allfeat);
+% Removing LBP78 as it has the same value for every sample
+tbl_featandlabels = removevars(tbl_featandlabels, {'LBP78'});
 
 % Feature selection 
 % Get features and labels out separately
@@ -41,8 +43,33 @@ bar(scores(idx(1:25)))
 xlabel('Predictor rank')
 ylabel('Predictor importance score')
 title("MRMR Feature Selection for CT Texture Features")
-top10 = [tbl_featandlabels(:,1:3) tbl_featandlabels(:,idx(1:10)+3)];
-writetable(top10, '../../Data/selectTextFeatures.xlsx');
 
-Mdl = fitrensemble(X, Y, 'Method', 'bag');
+top8 = [tbl_featandlabels(:,1:3) tbl_featandlabels(:,idx(1:8)+3)];
+writetable(top8, '../../Data/top8_TextureFeatures.xlsx');
+% top10 = [tbl_featandlabels(:,1:3) tbl_featandlabels(:,idx(1:10)+3)];
+% writetable(top10, '../../Data/selectTextFeatures.xlsx');
+% 
+% Mdl = fitrensemble(X, Y, 'Method', 'bag');
+
+% Feature selection using only training patients
+% train_pats = readtable("../../Data/train_pats.xlsx");
+% 
+% train_featandlabels = innerjoin(tbl_featandlabels, train_pats);
+% % train_featandlabels = removevars(train_featandlabels,{'LBP78'});
+% 
+% X1 = train_featandlabels(:,4:end);
+% Y1 = train_featandlabels.RFS;
+% 
+% [idx1, scores1] = fscmrmr(X1, Y1);
+% figure
+% bar(scores1(idx(1:25)))
+% xlabel('Predictor rank')
+% ylabel('Predictor importance score')
+% title("MRMR Feature Selection for CT Texture Features")
+% train_top10 = [train_featandlabels(:,1:3) train_featandlabels(:,idx(1:10)+3)];
+% % writetable(train_top10, '../../Data/train_TextureFeatures.xlsx');
+
+
+
+
 
