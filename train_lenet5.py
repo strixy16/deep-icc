e3a0337d11b916856f08ce664bfbb5514e23bd9c
Katy Scott <k.l.scott16@gmail.com>
2021-11-23 10:24:18 -0500

    Implementing cholangio binary prediction

--- train_lenet5.py
+++ train_lenet5.py
@@ -22,16 +22,15 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # parameters
 RANDOM_SEED = 42
-LEARNING_RATE = 0.001
-BATCH_SIZE = 32
+LEARNING_RATE = 0.003
+BATCH_SIZE = 16
 N_EPOCHS = 15
 
 IMG_SIZE = 32
 
-
-
 DATA_DIR = '/media/katy/Data/ICC/Data'
 SPLIT = 0.8
+
 
 def get_accuracy(model, data_loader, device):
     '''
@@ -44,8 +43,8 @@ def get_accuracy(model, data_loader, device):
     with torch.no_grad():
         model.eval()
         for X, y_true in data_loader:
-            X = X.to(device)
-            y_true = y_true.to(device)
+            X = X.float().to(device)
+            y_true = y_true.long().flatten().to(device)
 
             _, y_prob = model(X)
             _, predicted_labels = torch.max(y_prob, 1)
@@ -80,6 +79,8 @@ def plot_losses(train_losses, valid_losses):
     # change the plot style to default
     plt.style.use('default')
 
+    plt.show()
+
 
 def train(train_loader, model, criterion, optimizer, device):
     '''
@@ -91,22 +92,22 @@ def train(train_loader, model, criterion, optimizer, device):
 
     for X, y_true in train_loader:
 
-        ROW_IMG = 8
-        N_ROWS = 4
-
-        fig = plt.figure()
-        for index in range(1, ROW_IMG * N_ROWS + 1):
-            plt.subplot(N_ROWS, ROW_IMG, index)
-            plt.axis('off')
-            plt.imshow(X[index-1][0], cmap='gray_r')
-
-        fig.suptitle('Cholangio Dataset - preview')
-        plt.show()
+        # ROW_IMG = 4
+        # N_ROWS = 4
+        #
+        # fig = plt.figure()
+        # for index in range(1, ROW_IMG * N_ROWS + 1):
+        #     plt.subplot(N_ROWS, ROW_IMG, index)
+        #     plt.axis('off')
+        #     plt.imshow(X[index-1][0], cmap='gray_r')
+        #
+        # fig.suptitle('Cholangio Dataset - preview')
+        # plt.show()
 
         optimizer.zero_grad()
 
-        X = X.to(device)
-        y_true = y_true.to(device)
+        X = X.float().to(device)
+        y_true = y_true.long().flatten().to(device)
 
         # Forward pass
         y_hat, _ = model(X)
@@ -130,8 +131,8 @@ def validate(valid_loader, model, criterion, device):
     running_loss = 0
 
     for X, y_true in valid_loader:
-        X = X.to(device)
-        y_true = y_true.to(device)
+        X = X.float().to(device)
+        y_true = y_true.long().flatten().to(device)
 
         # Forward pass and record loss
         y_hat, _ = model(X)
@@ -180,22 +181,29 @@ def training_loop(model, criterion, optimizer, train_loader, valid_loader, epoch
     return model, optimizer, (train_losses, valid_losses)
 
 
-# Main that runs MNIST data
-if __name__ == '__main__':
+def train_cholangio():
     N_CLASSES = 2
+    SCAN_THRESH = 3
 
     info_path = os.path.join(DATA_DIR, 'Labels', 'bin_RFS_all_tumors_zero.csv')
     z_img_path = os.path.join(DATA_DIR, 'Images/Tumors', str(IMG_SIZE), 'Zero/')
 
     info = pd.read_csv(info_path)
 
-    patnum = np.asarray(info['Pat_ID'])
+    # Filter scans with mostly background in the image
+    filtered_indices = removeSmallScans(info, z_img_path, IMG_SIZE, SCAN_THRESH)
+    filtered_info = info.iloc[filtered_indices]
+
+    patnum = np.asarray(filtered_info['Pat_ID'])
     unique_patnum = np.unique(patnum)
+    label = np.asarray(filtered_info['RFS_Binary'])
 
-    train_idx, test_idx = train_test_split(unique_patnum, train_size=SPLIT, random_state=RANDOM_SEED, shuffle=True)
+    train_idx, test_idx = pat_train_test_split(patnum, label, SPLIT, seed=RANDOM_SEED)
 
-    train_dataset = CTBinSurvDataset(info, z_img_path, train_idx, IMG_SIZE)
-    test_dataset = CTBinSurvDataset(info, z_img_path, test_idx, IMG_SIZE)
+    # train_idx, test_idx = train_test_split(unique_patnum, train_size=SPLIT, random_state=RANDOM_SEED, shuffle=True)
+
+    train_dataset = CTBinSurvDataset(filtered_info, z_img_path, train_idx, IMG_SIZE)
+    test_dataset = CTBinSurvDataset(filtered_info, z_img_path, test_idx, IMG_SIZE)
 
     train_loader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE)
     test_loader = DataLoader(test_dataset, shuffle=True, batch_size=BATCH_SIZE)
@@ -210,44 +218,77 @@ if __name__ == '__main__':
     model, optimizer, _ = training_loop(model, criterion, optimizer, train_loader, test_loader, N_EPOCHS, DEVICE)
 
 
+
+
+if __name__ == '__main__':
+    train_cholangio()
+    # N_CLASSES = 10
+    # # define transforms
+    # # transforms.ToTensor() automatically scales images to [0,1] range
+    # transforms = transforms.Compose([transforms.Resize((32, 32)),
+    #                                  transforms.ToTensor()])
+    #
+    # # download and create datasets
+    # train_dataset = datasets.MNIST(root='/media/katy/Data/ICC/',
+    #                                train=True,
+    #                                transform=transforms,
+    #                                download=True)
+    #
+    # valid_dataset = datasets.MNIST(root='/media/katy/Data/ICC/',
+    #                                train=False,
+    #                                transform=transforms)
+    #
+    # # define the data loaders
+    # train_loader = DataLoader(dataset=train_dataset,
+    #                           batch_size=BATCH_SIZE,
+    #                           shuffle=True)
+    #
+    # valid_loader = DataLoader(dataset=valid_dataset,
+    #                           batch_size=BATCH_SIZE,
+    #                           shuffle=False)
+    #
+    # # Display data
+    # # ROW_IMG = 10
+    # # N_ROWS = 5
+    # #
+    # # fig = plt.figure()
+    # # for index in range(1, ROW_IMG * N_ROWS + 1):
+    # #     plt.subplot(N_ROWS, ROW_IMG, index)
+    # #     plt.axis('off')
+    # #     plt.imshow(train_dataset.data[index], cmap='gray_r')
+    # #
+    # # fig.suptitle('MNIST Dataset - preview')
+    # # plt.show()
+    #
+    # torch.manual_seed(RANDOM_SEED)
+    # model = LeNet5(N_CLASSES).to(DEVICE)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    # criterion = nn.CrossEntropyLoss()
+    #
+    # summary(model, input_size=(1, 32, 32), batch_size=BATCH_SIZE)
+    #
+    # model, optimizer, _ = training_loop(model, criterion, optimizer, train_loader, valid_loader, N_EPOCHS, DEVICE)
+
+
+# # Main that runs cholangio data
 # if __name__ == '__main__':
-#         N_CLASSES = 10
-#     # define transforms
-#     # transforms.ToTensor() automatically scales images to [0,1] range
-#     transforms = transforms.Compose([transforms.Resize((32, 32)),
-#                                      transforms.ToTensor()])
+#     N_CLASSES = 2
 #
-#     # download and create datasets
-#     train_dataset = datasets.MNIST(root='/media/katy/Data/ICC/',
-#                                    train=True,
-#                                    transform=transforms,
-#                                    download=True)
+#     info_path = os.path.join(DATA_DIR, 'Labels', 'bin_RFS_all_tumors_zero.csv')
+#     z_img_path = os.path.join(DATA_DIR, 'Images/Tumors', str(IMG_SIZE), 'Zero/')
 #
-#     valid_dataset = datasets.MNIST(root='/media/katy/Data/ICC/',
-#                                    train=False,
-#                                    transform=transforms)
+#     info = pd.read_csv(info_path)
 #
-#     # define the data loaders
-#     train_loader = DataLoader(dataset=train_dataset,
-#                               batch_size=BATCH_SIZE,
-#                               shuffle=True)
+#     patnum = np.asarray(info['Pat_ID'])
+#     unique_patnum = np.unique(patnum)
 #
-#     valid_loader = DataLoader(dataset=valid_dataset,
-#                               batch_size=BATCH_SIZE,
-#                               shuffle=False)
+#     train_idx, test_idx = train_test_split(unique_patnum, train_size=SPLIT, random_state=RANDOM_SEED, shuffle=True)
 #
-#     # Display data
-#     # ROW_IMG = 10
-#     # N_ROWS = 5
-#     #
-#     # fig = plt.figure()
-#     # for index in range(1, ROW_IMG * N_ROWS + 1):
-#     #     plt.subplot(N_ROWS, ROW_IMG, index)
-#     #     plt.axis('off')
-#     #     plt.imshow(train_dataset.data[index], cmap='gray_r')
-#     #
-#     # fig.suptitle('MNIST Dataset - preview')
-#     # plt.show()
+#     train_dataset = CTBinSurvDataset(info, z_img_path, train_idx, IMG_SIZE)
+#     test_dataset = CTBinSurvDataset(info, z_img_path, test_idx, IMG_SIZE)
+#
+#     train_loader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE)
+#     test_loader = DataLoader(test_dataset, shuffle=True, batch_size=BATCH_SIZE)
 #
 #     torch.manual_seed(RANDOM_SEED)
 #     model = LeNet5(N_CLASSES).to(DEVICE)
@@ -256,4 +297,4 @@ if __name__ == '__main__':
 #
 #     summary(model, input_size=(1, 32, 32), batch_size=BATCH_SIZE)
 #
-#     model, optimizer, _ = training_loop(model, criterion, optimizer, train_loader, valid_loader, N_EPOCHS, DEVICE)
+#     model, optimizer, _ = training_loop(model, criterion, optimizer, train_loader, test_loader, N_EPOCHS, DEVICE)
