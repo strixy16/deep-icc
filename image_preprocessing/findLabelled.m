72673465d3eff9d6a11cff6db58877cf9f86cff9
Katy Scott <k.l.scott16@gmail.com>
2021-12-06 12:20:54 -0500

    Updated to take conf file

--- image_preprocessing/findLabelled.m
+++ image_preprocessing/findLabelled.m
@@ -1,32 +1,39 @@
-
-all_file_dir = "../../Data/cholangio/AllSources/Tumor/";
-labels_file = "../../Data/RFS_Scout.xlsx";
-destination_dir = "../../Data/cholangio/labelled_only/tumors/";
-
-all_file_info = dir(strcat(all_file_dir, "*Tumor*"));
-
-tbl_all_file_info = struct2table(all_file_info);
-
-cell_all_file_names = natsort(tbl_all_file_info{:,'name'});
-
-img_labels = readtable(labels_file);
-
-files_to_move = cell(0, size(img_labels,1)*2);
-
-for label_idx = 1:size(img_labels, 1)
-    patient_ID = img_labels.ScoutID(label_idx);
+function findLabelled(conf_f)
     
-    patient_ID = strcat(patient_ID, '_');
-    
-    labelled_patient_idx = contains(cell_all_file_names, patient_ID);
-    
-    files_to_move = cell_all_file_names(labelled_patient_idx);
-    
-    for file_idx = 1:size(files_to_move, 1)
-        source = strcat(all_file_dir, files_to_move{file_idx});
-        destination = strcat(destination_dir, files_to_move{file_idx});
-        
-        status = copyfile(source, destination);
+    if ischar(conf_f) || isstring(conf_f)
+        conf_f = str2func(conf_f);
+        options = conf_f();
+    elseif isstruct(conf_f)
+        options = conf_f();
+    else
+        error("Input must be struct or name of .m config file")
     end
-    
+
+    all_file_info = dir(strcat(options.all_file_dir, "*Tumor*"));
+
+    tbl_all_file_info = struct2table(all_file_info);
+
+    cell_all_file_names = natsort(tbl_all_file_info{:,'name'});
+
+    img_labels = readtable(options.labels_file);
+
+    files_to_move = cell(0, size(img_labels,1)*2);
+
+    for label_idx = 1:size(img_labels, 1)
+        patient_ID = img_labels.ScoutID(label_idx);
+
+        patient_ID = strcat(patient_ID, '_');
+
+        labelled_patient_idx = contains(cell_all_file_names, patient_ID);
+
+        files_to_move = cell_all_file_names(labelled_patient_idx);
+
+        for file_idx = 1:size(files_to_move, 1)
+            source = strcat(options.all_file_dir, files_to_move{file_idx});
+            destination = strcat(options.destination_dir, files_to_move{file_idx});
+
+            status = copyfile(source, destination);
+        end
+
+    end
 end
