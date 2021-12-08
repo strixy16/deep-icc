64cc3a1e77bf77319169173e85140776e5551949
Katy Scott <k.l.scott16@gmail.com>
2021-12-08 15:17:25 -0500

    Renamed function to match purpose

--- image_preprocessing/generateMask.m
+++ image_preprocessing/generateMask.m
@@ -1,15 +1,16 @@
-function [Inew, dblMask] = new_ProcessImage(I)
+function intMask = generateMask(I)
     % Convert image to double
-    I = double(I);
+%     I = double(I);
     % Generate mask excluding values outside of this range (Hounsfield
     % units)
     % This range includes only soft tissue
     Imask = I<-100|I>300;
     % Flips black and white in the mask
     ImaskInv = imcomplement(Imask);
+    clear Imask I
     % Apply mask to image to separate tumor from background
-    Inew = I .* ImaskInv;
-    dblMask = double(ImaskInv);
+%     Inew = I .* ImaskInv;
+    intMask = int8(ImaskInv);
     % Make background NaN instead of 0s
 %     Inew(Inew==0) = NaN;
 %     out = Inew;
