371ab3fe22ae68e4fc2b11d894f820e09bb2b9c5
Katy Scott <k.l.scott16@gmail.com>
2022-01-05 14:43:37 -0500

    Kfold is running but model predicts 0 all the time

--- hdfs_train.py
+++ hdfs_train.py
@@ -3,11 +3,41 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
 import torch.cuda
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.optim as optim
 from torch.utils.data import DataLoader, SubsetRandomSampler
 
 from hdfs_data_loading import *
 from rfs_utils import *
 import hdfs_config as args
+
+
+class HDFSModel1(nn.Module):
+    def __init__(self):
+        super(HDFSModel1, self).__init__()
+        # ImgIn shape = (?, 1, 221, 221)
+        # Conv -> (?, 32, 108, 108)
+        # Pool -> (?, 32, 54, 54)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, stride=2)
+        # Conv -> (?, 16, 25, 25)
+        # Pool -> (?, 16, 12, 12)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=5, stride=2)
+
+        self.fc1 = nn.Linear(16*12*12, 32)
+        self.fc2 = nn.Linear(32, 1)
+        # self.fc3 = nn.Linear(3, 1)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
+        x = x.view(x.size(0), -1) # Flatten for FC
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc3(x))
+
+        return x
 
 
 def view_images(data_loader):
@@ -90,6 +120,35 @@ def kfold_train(data_info_path, data_img_path, k=None, seed=16):
 
         train_loader = DataLoader(dataset, batch_size=args.BATCH_SIZE, sampler=train_sampler)
         valid_loader = DataLoader(dataset, batch_size=args.BATCH_SIZE, sampler=valid_sampler, drop_last=True)
+
+        model = HDFSModel1()
+        model.to(device)
+        optimizer = optim.Adam(model.parameters(), lr=args.LR)
+        criterion = NegativeLogLikelihood(device)
+
+        history = {'train_loss': [], 'valid_loss': [], 'train_cind': [], 'valid_cind': []}
+        for epoch in range(args.EPOCHS):
+            train_loss, train_cind = train_epoch(model, device, train_loader, criterion, optimizer)
+            valid_loss, valid_cind = valid_epoch(model, device, valid_loader, criterion)
+
+            train_loss = train_loss / len(train_loader.sampler)
+            train_cind = train_cind / len(train_loader.sampler)
+            valid_loss = valid_loss / len(valid_loader.sampler)
+            valid_cind = valid_cind / len(valid_loader.sampler)
+
+            print("Epoch:{}/{} AVG Training Loss:{:.3f} AVG Validation Loss:{:.3f} "
+                  "AVG Training C-index:{:.2f} % AVG Validation C-index:{:.2f} %".format(epoch + 1,
+                                                                                         args.EPOCHS,
+                                                                                         train_loss,
+                                                                                         valid_loss,
+                                                                                         train_cind,
+                                                                                         valid_cind))
+            history['train_loss'].append(train_loss)
+            history['valid_loss'].append(valid_loss)
+            history['train_cind'].append(train_cind)
+            history['valid_cind'].append(valid_cind)
+
+        foldperf['fold{}'.format(fold+1)] = history
 
         print('breakpoint goes here')
 
