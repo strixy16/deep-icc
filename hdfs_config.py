6a349476e3c80dd55cf1a2128269c049e26dbeeb
Katy <k.l.scott16@gmail.com>
2022-02-09 20:08:41 +0000

    Updating file path construction to include cancer type and train test split

--- hdfs_config.py
+++ hdfs_config.py
@@ -1,15 +1,25 @@
 # Configuration file for use with HDFS training and testing
 
 # Data settings
+# Variables to set up file paths properly
+# Split percentage of data to use
+TEST_PERC = 10
+TRAIN_PERC = 100 - TEST_PERC
+
+# Cancer types to include in analysis (acronyms separated by _ as in the directories)
+CANCER_TYPES = "HCC_MCRC_ICC"
+
+# Dimension of CT image to load
+ORIG_IMG_DIM = 221
+
 # Full path to data directory where images, labels, etc. are stored and output will be saved
 DATA_DIR = '/Data/'
 # Path from data_dir to label files
-TRAIN_LABEL_FILE = 'Labels/HDFS_train_tumors.csv'
-TEST_LABEL_FILE = 'Labels/HDFS_test_tumors.csv'
+TRAIN_LABEL_FILE = 'Labels/' + CANCER_TYPES + '_HDFS_' + str(TRAIN_PERC) + '_' + str(TEST_PERC) + '_train_tumors.csv'
+TEST_LABEL_FILE = 'Labels/' + CANCER_TYPES + '_HDFS_' + str(TRAIN_PERC) + '_' + str(TEST_PERC) + '_test_tumors.csv'
 # Partial from data_dir to image files. From this directory select image dimension, then train and test folder
-IMG_LOC_PATH = 'Images/Labelled_Tumors/'
-# Dimension of CT image to load
-ORIG_IMG_DIM = 221
+IMG_LOC_PATH = 'Images/Labelled_Tumors/' + str(ORIG_IMG_DIM) + '/' + CANCER_TYPES + '_' + str(TRAIN_PERC) + '_' + str(TEST_PERC)
+
 
 MODEL_NAME = "HDFSModel2"
 
@@ -33,12 +43,11 @@ OPTIM_WEIGHT_DECAY = 0.0
 K = 5
 
 # Mode to run HDFS_train in
-TRAIN_MODE = False 
-LOAD_MODE = True
+TRAIN_MODE = True 
+LOAD_MODE = False
 
 # If just testing with existing model, put path to the model you wish to load here
-LOAD_MODEL_PATH = '/Data/Output/HDFSModel2/2022_02_03_0134/k_cross_HDFSModel2.pt'
-
+LOAD_MODEL_PATH = '/Data/Output/HDFSModel2/2022_02_03_2048_train/k_cross_HDFSModel2.pt'
 
 # Debugging
 # If true, prevents output from saving
