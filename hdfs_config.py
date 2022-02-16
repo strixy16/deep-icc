8e3b7c440d465a5d4e383ba26ccc449ec9a3e0d8
Katy <k.l.scott16@gmail.com>
2022-02-16 16:33:50 +0000

    Added comments to variables

--- hdfs_config.py
+++ hdfs_config.py
@@ -3,7 +3,7 @@
 # Data settings
 # Variables to set up file paths properly
 # Split percentage of data to use
-TEST_PERC = 10
+TEST_PERC = 20
 TRAIN_PERC = 100 - TEST_PERC
 
 # Cancer types to include in analysis (acronyms separated by _ as in the directories)
@@ -20,17 +20,25 @@ TEST_LABEL_FILE = 'Labels/' + CANCER_TYPES + '_HDFS_' + str(TRAIN_PERC) + '_' + 
 # Partial from data_dir to image files. From this directory select image dimension, then train and test folder
 IMG_LOC_PATH = 'Images/Labelled_Tumors/' + str(ORIG_IMG_DIM) + '/' + CANCER_TYPES + '_' + str(TRAIN_PERC) + '_' + str(TEST_PERC)
 
-
+# Model to train from hdfs_models
 MODEL_NAME = "HDFSModel2"
 
 # C-index to use - if true, uses GHCI 
 USE_GH = True
 
+# Random seed to set
 SEED = 16
 
+# Optimizer to use for training
 OPTIM = 'Adam'
+
+# Number of data to use in each minibatch
 BATCH_SIZE = 16
-EPOCHS = 30
+
+# Number of training epochs to run
+EPOCHS = 50
+
+# Initial learning rate
 LR = 0.0003
 
 # Regularization
@@ -43,7 +51,9 @@ OPTIM_WEIGHT_DECAY = 0.0
 K = 5
 
 # Mode to run HDFS_train in
+# Train mode creates a model and trains it from scratch using k-fold validation and tests the best fold
 TRAIN_MODE = True 
+# Load mode loads the trained model from LOAD_MODEL_PATH and runs it through testing
 LOAD_MODE = False
 
 # If just testing with existing model, put path to the model you wish to load here
@@ -51,4 +61,4 @@ LOAD_MODEL_PATH = '/Data/Output/HDFSModel2/2022_02_03_2048_train/k_cross_HDFSMod
 
 # Debugging
 # If true, prevents output from saving
-DEBUG = True
+DEBUG = False
