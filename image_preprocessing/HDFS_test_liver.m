9894517316a3bc75152ba5c1b090a556ea64fea2
Katy Scott <k.l.scott16@gmail.com>
2022-03-09 11:18:59 -0500

    Made train/test config files for liver

---
+++ image_preprocessing/HDFS_test_liver.m
@@ -0,0 +1,17 @@
+function opt = HDFS_test_liver
+    HDFS_liver_options = all_HDFS_liver;
+    opt.TestSize = HDFS_liver_options.TestSize;
+    
+    test_perc = opt.TestSize * 100;
+    train_perc = 100 - (test_perc);
+    
+    opt.BinLoc = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Images/Labelled_Liver/412/HCC_MCRC_ICC_", ...
+        string(train_perc),"_", string(test_perc),"/test/");
+    opt.CSVname = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Labels/Liver/HCC_MCRC_ICC_HDFS_", ...
+        string(train_perc),"_", string(test_perc),"_test_liver.csv");
+    opt.CSV_header = {'File', 'Pat_ID', 'Slice_Num', 'HDFS_Code', 'HDFS_Time'};
+    
+    opt.Labels = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Labels/Liver/HCC_MCRC_ICC_HDFS_liver_", ...
+        string(train_perc),"_", string(test_perc),"_test.xlsx");
+
+end
--- image_preprocessing/all_HDFS_liver.m
+++ image_preprocessing/all_HDFS_liver.m
@@ -13,19 +13,33 @@ function opt = all_HDFS_liver
     opt.ImageSize = [412 412];
     
     % Location of image files for tumor image set
-    opt.ImageLoc = "../../Data/All_CT/Liver/";
+%     opt.ImageLoc = "../../Data/All_CT/Liver/";
     
     % Location of bin folder to output tumor image slice set at end of
     % new_preprocessMHA
-    opt.BinLoc = strcat("../../Data/Images/Labelled_Liver/", string(opt.ImageSize(1)), "/Original/");
+    opt.BinLoc = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Images/Labelled_Liver/", string(opt.ImageSize(1)), "/Original/");
     
     % Output CSV setup for createCSV
-    opt.CSVname = "../../Data/Labels/HCC_MCRC_ICC_HDFS_labelled_liver.csv";
+    opt.CSVname = "/Users/katyscott/Desktop/HDFS_Project/Data/Labels/Liver/HCC_MCRC_ICC_HDFS_labelled_liver.csv";
     opt.CSV_header = {'File', 'Pat_ID', 'Slice_Num', 'HDFS_Code', 'HDFS_Time'};
     
-    opt.Labels = "/home/katy/Data/ICC/HDFS/HCC_MCRC_ICC_HDFS_liver_labels.xlsx";
+    opt.Labels = "/Users/katyscott/Desktop/HDFS_Project/Data/Labels/Liver/HCC_MCRC_ICC_HDFS_liver_labels.xlsx";
     
     % String to use to find the correct image type in directories
     opt.search_string = "*iver*";
     
+    % Test percentage for train-test split
+    opt.TestSize = 0.20;
+    
+    test_perc = opt.TestSize * 100;
+    train_perc = 100 - (test_perc);
+    
+    % Output spreadsheets with patient level train and test labels
+    opt.TrainLabels = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Labels/Liver/HCC_MCRC_ICC_HDFS_liver_", string(train_perc),"_", string(test_perc),"_train.xlsx");
+    opt.TestLabels = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Labels/Liver/HCC_MCRC_ICC_HDFS_liver_", string(train_perc),"_", string(test_perc),"_test.xlsx");
+    
+    % Directories for train and test bin images
+    opt.TrainDestination = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Images/Labelled_Liver/412/HCC_MCRC_ICC_", string(train_perc),"_", string(test_perc),"/train/");
+    opt.TestDestination = strcat("/Users/katyscott/Desktop/HDFS_Project/Data/Images/Labelled_Liver/412/HCC_MCRC_ICC_", string(train_perc),"_", string(test_perc),"/test/");
+     
 end
